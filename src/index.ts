338c1b2cee3bca62f5226f5cf01e87cb515ec406
leirong <643609270@qq.com>
2024-09-03 21:36:26 +0800

    feat:add Mathematical operations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { add } from './utils';
+export { add, subtract, multiply, divide } from './utils';
--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,8 +1,26 @@
-import { add } from './utils';
+import { add, subtract, multiply, divide } from './utils';
 import { test, expect } from 'vitest';
 
 test('add', () => {
   expect(add(1, 2)).toBe(3);
   expect(add(2, 2)).toBe(4);
   expect(add(3, 2)).toBe(5);
+});
+
+test('subtract', () => {
+  expect(subtract(1, 2)).toBe(-1);
+  expect(subtract(2, 2)).toBe(0);
+  expect(subtract(3, 2)).toBe(1);
+});
+
+test('multiply', () => {
+  expect(multiply(1, 2)).toBe(2);
+  expect(multiply(2, 2)).toBe(4);
+  expect(multiply(3, 2)).toBe(6);
+});
+
+test('divide', () => {
+  expect(divide(1, 2)).toBe(0.5);
+  expect(divide(2, 2)).toBe(1);
+  expect(divide(3, 2)).toBe(1.5);
 });
